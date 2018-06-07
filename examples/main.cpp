134ac48b688c0d1570007328e1f6579b5e747be7
Dan Tull <dtull@adobe.com>
2018-06-07 12:56:57 -0500

    Add diagnostic line for future use.

--- examples/main.cpp
+++ examples/main.cpp
@@ -15,6 +15,8 @@ int main(int argc, char **argv) {
         target.set_feature(Halide::Target::Feature::UserContext);
 
         theFunc.compile_to_object(argv[1] + std::string(".o"), arguments, argv[2], target);
+        // handy line of code for diagnosing changes to the API to a generated library
+        // theFunc.compile_to_header(argv[1] + std::string("_Header.h"), arguments, argv[2], target);
 
         return 0;
     }
